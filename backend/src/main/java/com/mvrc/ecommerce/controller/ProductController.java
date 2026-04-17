dfeff25f166cac7f289d25af212c5429180081b1
MarcosCantelli <marcoscantelli@hotmail.com>
2026-04-17 16:38:58 -0300

    arruamndo erro do controller

--- backend/src/main/java/com/mvrc/ecommerce/controller/ProductController.java
+++ backend/src/main/java/com/mvrc/ecommerce/controller/ProductController.java
@@ -8,9 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(" docker exec -it ecommerce sh\r\n" + //
-        "/ # grep -r \"localhost:8080\" /usr/share/nginx/html\r\n" + //
-        "")
+@RequestMapping("/api/products")
 @CrossOrigin(origins = "*")
 @RequiredArgsConstructor
 public class ProductController {
